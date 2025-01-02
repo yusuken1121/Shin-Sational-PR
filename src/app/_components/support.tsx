ae9011e0ebc19e8b3194140d2555f1b6ab927ba7
yusuken1121 <yusuken1121@gmail.com>
2025-01-01 18:23:24 -0800

    installation-stepsの小文字を大文字に変更

--- src/app/_components/support.tsx
+++ src/app/_components/support.tsx
@@ -10,10 +10,10 @@ export function Support() {
   ];
 
   return (
-    <section className="py-24 bg-primary">
+    <section className="flex items-center justify-center w-full h-full py-24 bg-primary">
       <div className="container px-4 md:px-6">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
-          サポート内容
+          Support Contents
         </h2>
         <Card className="max-w-lg mx-auto bg-zinc-800 border-zinc-700">
           <CardContent className="p-6">
