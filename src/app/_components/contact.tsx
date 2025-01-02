276b059863a75c61f4ec518044b1c4242017d31b
yusuken1121 <yusuken1121@gmail.com>
2025-01-01 20:31:13 -0800

    Contactフォームにバリデーションを追加

--- src/app/_components/contact.tsx
+++ src/app/_components/contact.tsx
@@ -1,40 +1,123 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Name must be at least 3 characters long")
+    .max(100, "Name cannot exceed 100 characters"),
+  email: z.string().email("Invalid email address"),
+  message: z
+    .string()
+    .min(20, "Message must be at least 20 characters long")
+    .max(1000, "Message cannot exceed 1000 characters"),
+});
 
 export function Contact() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <section className="flex items-center justify-center w-full h-full py-24">
       <div className="container px-4 md:px-6">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
           Contact
         </h2>
-        <div className="max-w-lg mx-auto">
-          <form className="space-y-6">
-            <div>
-              <Input
-                placeholder="Name"
-                className="bg-primary border-zinc-700"
-              />
-            </div>
-            <div>
-              <Input
-                type="email"
-                placeholder="Mail Address"
-                className="bg-primary border-zinc-700"
-              />
-            </div>
-            <div>
-              <Textarea
-                placeholder="Message"
-                className="bg-primary border-zinc-700"
-              />
-            </div>
-            <Button type="submit" variant="default" className="w-full">
-              Send
-            </Button>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 max-w-lg mx-auto"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    {/* <Input placeholder="shadcn" /> */}
+                    <Input
+                      placeholder="John Doe"
+                      className="bg-primary border-zinc-700"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="hidden">
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email Address</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="sample@sample.com"
+                      className="bg-primary border-zinc-700"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="hidden">
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Type your message here"
+                      className="bg-primary border-zinc-700"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="hidden">
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
           </form>
-        </div>
+        </Form>
       </div>
     </section>
   );
