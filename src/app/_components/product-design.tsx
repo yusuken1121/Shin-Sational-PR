4eabd43d4dc858eb9ee6fc64b7531237c404c816
yusuken1121 <yusuken1121@gmail.com>
2025-01-02 19:04:29 -0800

    カルーセルのデザインを修正

--- src/app/_components/product-design.tsx
+++ src/app/_components/product-design.tsx
@@ -7,20 +7,21 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Image from "next/image";
 
 const laptopDesigns = [
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
+  { title: "menu", img: "design1.jpg" },
+  { title: "menu", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
 ];
 const mobileDesigns = [
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
-  { img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
+  { title: "homepage", img: "design1.jpg" },
 ];
 
 export function ProductDesign() {
@@ -30,32 +31,76 @@ export function ProductDesign() {
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
           Designs
         </h2>
-        <div className="flex items-center justify-center w-full">
+        <div className="flex flex-col items-center justify-center space-y-10 w-full">
           {/* ラップトップ版 */}
-          <Carousel className="w-4/5">
-            <CarouselContent className="-ml-1">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem
-                  key={index}
-                  className="pl-1 md:basis-2/3 lg:basis-4/5"
-                >
-                  <div className="p-1">
-                    <Card>
-                      <AspectRatio ratio={16 / 9}>
-                        <CardContent className="flex w-full h-full items-center justify-center p-6">
-                          <span className="text-2xl font-semibold">
-                            {index + 1}
-                          </span>
-                        </CardContent>
-                      </AspectRatio>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious variant="default" />
-            <CarouselNext variant="default" />
-          </Carousel>
+          <div className="w-full flex flex-col items-center justify-center gap-3">
+            <h3 className="text-xl text-accent">Laptop</h3>
+            <Carousel className="w-4/5">
+              <CarouselContent className="-ml-1">
+                {laptopDesigns.map((design, index) => (
+                  <CarouselItem
+                    key={index}
+                    className="pl-1 md:basis-2/3 lg:basis-4/5"
+                  >
+                    <h4 className="flex items-center justify-center">
+                      {design.title}
+                    </h4>
+                    <div className="p-1">
+                      <Card>
+                        <AspectRatio ratio={16 / 9}>
+                          {/* 画像を表示したい場合は以下のように img タグを使用 */}
+                          <Image
+                            src={design.img}
+                            alt={design.title}
+                            fill={true}
+                            className="w-full h-full object-cover"
+                            unoptimized={true}
+                          />
+                        </AspectRatio>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious variant="default" />
+              <CarouselNext variant="default" />
+            </Carousel>
+          </div>
+
+          {/* 携帯版 */}
+          <div className="w-full flex flex-col items-center justify-center gap-3">
+            <h3 className="text-xl text-accent">Mobile</h3>
+            <Carousel className="w-4/5">
+              <CarouselContent className="-ml-1">
+                {mobileDesigns.map((design, index) => (
+                  <CarouselItem
+                    key={index}
+                    className="pl-1 md:basis-2/3 lg:basis-4/5"
+                  >
+                    <h4 className="flex items-center justify-center">
+                      {design.title}
+                    </h4>
+                    <div className="flex justify-center items-center p-1">
+                      <Card className="w-[56%]">
+                        <AspectRatio ratio={9 / 16}>
+                          {/* 画像を表示したい場合は以下のように img タグを使用 */}
+                          <Image
+                            src={design.img}
+                            alt={design.title}
+                            fill={true}
+                            className="w-full h-full object-cover"
+                            unoptimized={true}
+                          />
+                        </AspectRatio>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious variant="default" />
+              <CarouselNext variant="default" />
+            </Carousel>
+          </div>
         </div>
       </div>
     </section>
